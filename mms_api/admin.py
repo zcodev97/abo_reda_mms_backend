3ad51149a4f1e8f5039a9426e8a70a63a7997e0c
zakriabilal <zcodev97@gmail.com>
2024-02-11 15:38:11 +0300

    update admin site, remove multi deletion, hide id for each model

--- mms_api/admin.py
+++ mms_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import (Deposit, Withdraw, WithdrawType,
                      Company, Container)
+from django.utils.html import format_html
 
 
 # Register your models here.
@@ -13,17 +14,43 @@ from .models import (Deposit, Withdraw, WithdrawType,
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'container', 'total_dinar',
-                    'total_dollar', 'created_at', 'created_by']
+    def formatted_total_price_dinar(self, obj):
+        return format_html(f"IQD {obj.total_dinar:,.0f}")
+    def formatted_total_price_dollar(self, obj):
+        return format_html(f"$ {obj.total_dollar:,.0f}")
+
+    formatted_total_price_dinar.short_description = 'Total Dinar'  # Sets the column header
+
+    formatted_total_price_dollar.short_description = 'Total Dollar'  # Sets the column header
+
+    list_display = [ 'title', 'container', 'formatted_total_price_dinar',
+                    'formatted_total_price_dollar', 'created_at', 'created_by']
+
+    def get_actions(self, request):
+        actions = super(CompanyAdmin,self).get_actions(request)
+        print(actions)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
 
 
 @admin.register(Container)
 class ContainerAdmin(admin.ModelAdmin):
+
+
     list_per_page = 5  # Items per page
     ordering = ('-created_at',)  # Default ordering
     search_fields = ['name'] # Fields to search by
-    list_display = ['id', 'name', 'total_dinar', 'total_dollar',
+    list_display = [ 'name', 'total_dinar', 'total_dollar',
                     'created_at', 'created_by']
+
+    def get_actions(self, request):
+        actions = super(ContainerAdmin,self).get_actions(request)
+        print(actions)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
 
 
 @admin.register(Deposit)
@@ -40,6 +67,13 @@ class DepositAdmin(admin.ModelAdmin):
                     'description', 'received_from',
                     'created_at', 'created_by']
 
+    def get_actions(self, request):
+        actions = super(DepositAdmin,self).get_actions(request)
+        print(actions)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
 
 @admin.register(Withdraw)
 class WithdrawAdmin(admin.ModelAdmin):
@@ -47,18 +81,32 @@ class WithdrawAdmin(admin.ModelAdmin):
     ordering = ('-created_at',)  # Default ordering
     search_fields = ( 'withdraw_type__title',
                     'container__name', 'company_name__title','description','out_to') # Fields to search by
-    list_display = ['id', 'invoice_id', 'withdraw_type',
+    list_display = [ 'invoice_id', 'withdraw_type',
                     'container', 'company_name',
                     'price_in_dinar', 'price_in_dollar',
                     'description', 'out_to',
                     'created_at', 'created_by']
 
+    def get_actions(self, request):
+        actions = super(WithdrawAdmin,self).get_actions(request)
+        print(actions)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
 
 @admin.register(WithdrawType)
 class WithdrawTypeAdmin(admin.ModelAdmin):
     list_per_page = 5  # Items per page
     search_fields = ['title'] # Fields to search by
-    list_display = ['id', 'title']
+    list_display = [ 'title']
+
+    def get_actions(self, request):
+        actions = super(WithdrawTypeAdmin,self).get_actions(request)
+        print(actions)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
 
 # @admin.register(LogEntry)
 # class LogEntryAdmin(admin.ModelAdmin):
